ca289e757af9099870a1bd1b9bcec94e493ea3e4
kmazurczyk <140034899+kmazurczyk@users.noreply.github.com>
2024-10-16 22:00:37 -0600

    MTA EDA, progress on Property Valuations EDA, Stabilized APIs and IO

--- data/SubwayStops_API.py
+++ data/SubwayStops_API.py
@@ -1,13 +1,27 @@
 from configparser import ConfigParser
+from datetime import date, timedelta
+import time
 import requests
 import json
 import pandas as pd
+import os
+
+# SEARCH FOR AND SAVE FILES WITH RESPECT TO ROOT PROJECT FOLDER
+def get_base_path():
+    cwd = os.path.basename(os.getcwd())
+    if cwd.lower() != 'airbnb-price-predictor':
+        return os.path.dirname(os.getcwd())
+    else:
+        return os.getcwd()
 
 config = ConfigParser()
-config.read('../config.ini')
+config_path = get_base_path() + '/config.ini'
+# print("configuring from", config_path)
+config.read(config_path)
 
-# save output to
-csv_file_path = config['DATA_SOURCES']['subway_csv']
+# FILE I/O
+data_dir = get_base_path() + config['DATA_SOURCES']['data_dir']
+csv_file_name = config['DATA_SOURCES']['subway_csv']
 
 def call_api():
 
@@ -36,4 +50,4 @@ def write_csv(df, file_path):
 
 if __name__ == '__main__':
     data = call_api()
-    write_csv(data, csv_file_path + '.csv')
+    write_csv(data, data_dir + csv_file_name + '.csv')
